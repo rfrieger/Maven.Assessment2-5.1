1d011fba33e16cc884f498a5ba53e51160debcdf
rfrieger <rfrieger@gmail.com>
2019-06-17 12:32:01 -0400

    before lunchclear

--- src/main/java/rocks/zipcode/assessment2/arrays/StringArrayUtils.java
+++ src/main/java/rocks/zipcode/assessment2/arrays/StringArrayUtils.java
@@ -12,7 +12,8 @@ public class StringArrayUtils {
      * @param endingIndex - ending index of array
      * @return an array with all elements between `startingIndex` and `endingIndex`
      */
-    public static String[] getSubArray(String[] arrayToBeSpliced, int startingIndex, int endingIndex) {
+    public static String[] getSubArray(String[] arrayToBeSpliced, int startingIndex, int endingIndex) throws IllegalArgumentException {
+
         ArrayList<String> arr = new ArrayList();
         for (int i = startingIndex; i < endingIndex ; i++) {
             arr.add(arrayToBeSpliced[i]);
@@ -31,7 +32,7 @@ public class StringArrayUtils {
      * @param startingIndex - starting index of array to be spliced
      * @return an array all elements between after `startingIndex`
      */
-    public static String[] getEndingArray(String[] arrayToBeSpliced, int startingIndex) {
+    public static String[] getEndingArray(String[] arrayToBeSpliced, int startingIndex) throws IllegalArgumentException {
         ArrayList<String> arr = new ArrayList();
         for (int i = startingIndex; i < arrayToBeSpliced.length ; i++) {
             arr.add(arrayToBeSpliced[i]);
